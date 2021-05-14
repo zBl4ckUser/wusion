c4f0b17d86c2426b14da5e4c282a75ef8cf12d55
zBl4ckUser <zBl4ckDev@protonmail.com>
2021-05-13 22:58:38 -0300

    Change coin unit

--- src/qt/bitcoinunits.cpp
+++ src/qt/bitcoinunits.cpp
@@ -43,9 +43,9 @@ QString BitcoinUnits::longName(int unit)
     switch(unit)
     {
     case BTC: return QString("WUS");
-    case mBTC: return QString("lites");
-    case uBTC: return QString("photons");
-    case SAT: return QString("litoshi");
+    case mBTC: return QString("mWus");
+    case uBTC: return QString("uWUS");
+    case SAT: return QString("Photon");
     default: return QString("???");
     }
 }
@@ -64,10 +64,10 @@ QString BitcoinUnits::description(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("Wusions");
-    case mBTC: return QString("Lites (1 / 1" THIN_SP_UTF8 "000)");
-    case uBTC: return QString("Photons (1 / 1" THIN_SP_UTF8 "000" THIN_SP_UTF8 "000)");
-    case SAT: return QString("Litoshis (sat) (1 / 100" THIN_SP_UTF8 "000" THIN_SP_UTF8 "000)");
+    case BTC: return QString("WUS");
+    case mBTC: return QString("mWUS (1 / 1" THIN_SP_UTF8 "000)");
+    case uBTC: return QString("uWUS (1 / 1" THIN_SP_UTF8 "000" THIN_SP_UTF8 "000)");
+    case SAT: return QString("Photon (sat) (1 / 100" THIN_SP_UTF8 "000" THIN_SP_UTF8 "000)");
     default: return QString("???");
     }
 }
